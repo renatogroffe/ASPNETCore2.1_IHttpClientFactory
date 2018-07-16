bf4a70bea933b360dfef6d9c6aa81e287dc7021c
Renato Groffe <renato.groffe@yahoo.com.br>
2018-07-15 23:47:31 -0300

    ASP.NET Core 2.1 + IHttpClientFactory
    ASP.NET Core 2.1 + IHttpClientFactory

---
+++ NamedClient/ExemploHttpClientFactory/Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace ExemploHttpClientFactory.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet(
+            [FromServices]IHttpClientFactory httpClientFactory)
+        {
+            DateTime dataBase = DateTime.Now.Date.AddDays(
+                new Random().Next(0, 7) * -1);
+
+            var client = httpClientFactory.CreateClient("NASA_API");
+
+            var response = client.GetAsync(
+                client.BaseAddress +
+                $"&date={dataBase.ToString("yyyy-MM-dd")}").Result;
+
+            response.EnsureSuccessStatusCode();
+            string conteudo =
+                response.Content.ReadAsStringAsync().Result;
+            dynamic resultado =
+                JsonConvert.DeserializeObject(conteudo);
+
+            ImagemNASA imagem = new ImagemNASA();
+            imagem.Data = dataBase;
+            imagem.Titulo = resultado.title;
+            imagem.Descricao = resultado.explanation;
+            imagem.Url = resultado.url;
+            imagem.MediaType = resultado.media_type;
+
+            TempData["ImagemNASA"] = imagem;
+        }       
+    }
+}
---
+++ TypedClient/ExemploHttpClientFactory/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExemploHttpClientFactory.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet([FromServices]APINasaClient client)
+        {
+            TempData["ImagemNASA"] = client.ObterDadosImagem();
+        }
+    }
+}
